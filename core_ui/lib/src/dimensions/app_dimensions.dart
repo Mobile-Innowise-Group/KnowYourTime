82e597ad90dec72402a7da5a16d4d763bb48e363
Yury Fyodorov <yury.fiodarau@innowise-group.com>
2022-08-17 10:30:59 +0300

    Split login screen into web and mobile, add firebase options

--- core_ui/lib/src/dimensions/app_dimensions.dart
+++ core_ui/lib/src/dimensions/app_dimensions.dart
@@ -7,6 +7,8 @@ class AppDimensions {
   static const double padding10 = 10.0;
   static const double padding20 = 20.0;
   static const double padding24 = 24.0;
+  static const double padding60 = 60.0;
+  static const double padding70 = 70.0;
   static const double padding100 = 100.0;
 
   static const double imageSize40 = 40.0;
